3b444b0cf319fb2c440db03457a8711c30eab34e
dasmarandei <diana.e.asmarandei@gmail.com>
2021-07-19 22:22:04 +0300

    Initial commit

---
+++ src/test/java/TestClasses.java
@@ -0,0 +1,11 @@
+import org.junit.Test;
+
+
+public class TestClasses {
+
+    @Test
+    public void Test01() {
+        System.out.println("This is my very first test!");
+    }
+
+}
---
+++ src/test/java/TestNgClasses.java
@@ -0,0 +1,10 @@
+import org.testng.annotations.Test;
+
+public class TestNgClasses {
+
+    @Test
+    public void Test01(){
+        System.out.println("My very first TestNg test");
+
+    }
+}
